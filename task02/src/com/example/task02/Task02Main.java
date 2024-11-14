034d2c0813f0b6aaf0acc3b2dd2e461e41042b5e
Soniccooles <andrey.sonic@mail.ru>
2024-11-14 20:54:28 +0500

    task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,38 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int current;
+        boolean carried = false;
+        while ((current = System.in.read()) != -1)
+        {
+            if (current == 13) { // встретили 13
+                if (carried) { // если прошлый был 13, то пишем 13 (r)
+                    System.out.write(13);
+                }
+                carried = true; // ставим carried в true
+            }
+            else if (current == 10) {
+                if (carried) {
+                    System.out.write(10); // если прошлое было 13 (флаг carried = true), тогда выводим только 10 (r\n\ -> n\)
+                    carried = false; // ставим carried в false
+                }
+                else {
+                    System.out.write(current); // если прошлое не 13, то просто выводим 10 (n)
+                }
+            }
+            else {
+                if (carried) { // если в целом другой байт и прошлое это 13, то выводим и 13, и этот байт
+                    System.out.write(13);
+                    carried = false;
+                }
+                System.out.write(current);
+            }
+
+        }
+        if (carried)
+        {
+            System.out.write(13); // на случай, если последний байт это \r, то есть 13
+        }
+        System.out.flush(); // очищаем буфер
     }
 }
